376d11d6996f7f39422a59e67242e8fb6292ed17
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-17 00:43:46 +0530

    update area of triangle completed

--- area-of-triangle.js
+++ area-of-triangle.js
@@ -2,3 +2,15 @@ const inputs = document.querySelectorAll(".triangle-input");
 const btnArea = document.querySelector("#btn-area");
 const message = document.querySelector(".message");
 
+btnArea.addEventListener("click", calculateAreaOfTriangle);
+
+function productOfBaseAndHeight(a,b){
+    const productOfBaseAndHeight = a*b;
+    return productOfBaseAndHeight;
+}
+
+function calculateAreaOfTriangle() {
+    const product = productOfBaseAndHeight(Number(inputs[0].value), Number(inputs[1].value));
+    const areaOfTriangle = product / 2;
+    message.innerText = `Area of Triangle is ${areaOfTriangle} cm²`;
+}
