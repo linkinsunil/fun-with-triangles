df5c2d915dbef747cb4725816f89b655bf2a87a9
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-16 19:15:02 +0530

    update: selected elements in js

---
+++ quiz.js
@@ -0,0 +1,5 @@
+const quizForm = document.querySelector(".quiz-form")
+const submitAnswerBtn = document.querySelector("#submit-answer-button")
+const message = document.querySelector("#message")
+
+const answers = ["90°", "right angled"];
