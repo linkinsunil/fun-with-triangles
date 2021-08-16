512bdb29a1f94081c351084caaf0edbb174dcbca
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-16 22:09:04 +0530

    wip finalizing quiz

--- quiz.js
+++ quiz.js
@@ -1,5 +1,22 @@
 const quizForm = document.querySelector(".quiz-form")
 const submitAnswerBtn = document.querySelector("#submit-answer-button")
 const message = document.querySelector("#message")
+const correctAnswers = ["90°", "right angled"];
 
-const answers = ["90°", "right angled"];
+function calculateScore(){
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    // debugger;
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score = score + 1;
+           // message.innerText = `Correct, your score is ${score}`;
+        }
+        index = index + 1;
+    }
+    console.log(score);
+    message.innerText = `Your score is ${score}`;
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore)
