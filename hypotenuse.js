5e94be1cb7e43deb1bf127b94abd770dff2257c1
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-16 23:52:12 +0530

    wip task complete

---
+++ hypotenuse.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll(".side-input")
+const btnHypotenuse = document.querySelector("#btn-hypotenuse")
+const message = document.querySelector("#message")
+
+function calculateHypotenuse(){
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+    const lengthOfHypotenuse = (Math.sqrt(sumOfSquares))
+    message.innerText = `Length of Hypotenuse is ${lengthOfHypotenuse}`;
+}
+
+function calculateSumOfSquares(a,b){
+    const sumOfSquares = a*a + b*b;
+    return sumOfSquares;
+}
+
+btnHypotenuse.addEventListener("click", calculateHypotenuse);
