8fb73292c9bee6236e6f353ca7fc490025e81760
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-19 16:58:47 +0530

    update: added toFixed method in output display

--- hypotenuse.js
+++ hypotenuse.js
@@ -5,7 +5,7 @@ const message = document.querySelector("#message")
 function calculateHypotenuse(){
     const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
     const lengthOfHypotenuse = (Math.sqrt(sumOfSquares))
-    message.innerText = `Length of Hypotenuse is ${lengthOfHypotenuse}`;
+    message.innerText = `Length of Hypotenuse is ${lengthOfHypotenuse.toFixed(2)}`;
 }
 
 function calculateSumOfSquares(a,b){
